1e7163802a39dacc42b317433508de9de106838b
jramso <josue.rsou2@gmail.com>
2022-10-22 23:18:53 -0300

    fix(macroBloco()): changed macroblock calculation

--- TrabPthreads/Main.c
+++ TrabPthreads/Main.c
@@ -8,21 +8,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 /* CONSTANTES QUE NAO SAO CONSTANTES */
-#define NUM_THREADS 1
+#define NUM_THREADS 2
 #define LINHA 100
 #define COLUNA 100
 #define MACRO_LINHA 5
 #define MACRO_COLUNA 5
+#define COLFIL (COLUNA/MACRO_COLUNA)
 /* GLOBAL VARS */
 int soma;
 int** matriz;
+int** macro;
+
+
 
 
 /*  REF METODOS */
+//int calcPrimo(int n);
 void* calcPrimo(void*);
-int **criaMatriz();
+
+void criaMatriz();
+int** macroBloco();
 
 /* MAIN */
 int main(int argc, char* argv[]) {
@@ -32,12 +40,28 @@ int main(int argc, char* argv[]) {
 		for (int j = 0; j < COLUNA; j++) {
 			printf(" %d ", matriz[i][j]);
 		}
+		printf("\n");
 	}
+		printf("\n");
+		printf("\n");
+
+	macroBloco(400);
+	for (int i = 0; i < MACRO_LINHA; i++) {
+		for (int j = 0; j < MACRO_COLUNA; j++) {
+			printf(" %d ", macro[i][j]);
+		}
+		printf("\n");
+	}
+
+	
 	pthread_t thread[NUM_THREADS];
 	/* CRIAÇÃO DAS THREADS */
-	int parametro = 10;
-	for (int i = 0; i < NUM_THREADS; i++) {
-		pthread_create(&thread[i], NULL, calcPrimo, &parametro);
+	for (int k = 0; k < 10; k++) {
+		int parametro = matriz[0][k];
+		for (int i = 0; i < NUM_THREADS; i++) {
+			pthread_create(&thread[i], NULL, calcPrimo, &parametro);
+		}
+
 	}
 	/* JOIN NAS THREADS*/
 	for (int j = 0; j < NUM_THREADS; j++) {
@@ -52,51 +76,69 @@ int main(int argc, char* argv[]) {
 
 /* METODOS */
 // 1. metodo que confere se um numero é primo
+//int calcPrimo(int n) {
 void* calcPrimo(void* param) {
-	int num = *((int*)param);
+	int n = *((int*)param);
 	int cont = 0;
-	for (int i = 1; i <= num; i++) {
-		if (num % i == 0) {
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (n % i == 0) {
 			cont++;
 		}
 	}
-	if (cont == 2) {
+	if (cont == 0) {
 		soma++;
+		printf("\nO numero %d E PRIMO\n\n", n);
 	}
 	else {
-		printf("\nO numero %d NAO e primo\n\n", num);
+		printf("\nO numero %d NAO e primo\n\n", n);
 	}
 	pthread_exit(0);
 }//fim calcPrimo
 
 // 2. metodo que aloca memória e preenche a matriz
 
-int** criaMatriz() {
+void criaMatriz() {
 
 	int i, j;
 
-	//vetor de LINHAS ponteiros
+	// aloca um vetor de LIN ponteiros para linhas
 	matriz = malloc(LINHA * sizeof(int*));
-	//vetor de todos os elementos da matriz
-	matriz[0] = malloc(LINHA * COLUNA * sizeof(int));
-	//ajusta os ponteiros de linhas onde i>0
-	for (i = 1; i < LINHA; i++) {
-		for (j = 0; j < COLUNA; j++) {
-			matriz[i] = matriz[0] + i * COLUNA;
-		}
-	}
+
+	// aloca cada uma das linhas (vetores de COL inteiros)
+	for (i = 0; i < LINHA; i++)
+		matriz[i] = malloc(COLUNA * sizeof(int));
+
 	//percorre e preenche a matriz
 	for (int i = 0; i < LINHA; i++) {
 		for (int j = 0; j < COLUNA; j++) {
-			matriz[i][j] = rand() % 3199;
+			//matriz[i][j] = rand() % 32000;
+			matriz[i][j] = i;
 		}
 	}
 }//fim criaMatriz()
 
 // 3. metodo que cria as submatrizes "macroblocos"
 
-int macroBloco() {
-	int** macro;
+int** macroBloco(int n) {
+	//numero do macrobloco
+	int lin, col;
+	macro = malloc(MACRO_LINHA * sizeof(int*));
+	macro[0] = malloc(MACRO_LINHA * MACRO_COLUNA * sizeof(int));
+	//ajusta os ponteiros de linhas onde i>0
+	for (lin = 1; lin < MACRO_LINHA; lin++) {
+		for (col = 0; col < MACRO_COLUNA; col++) {
+			macro[lin] = macro[col] + lin * MACRO_COLUNA;
+		}
+	}
 
+	//calculo de onde o macrobloco entra na matriz
+	for (lin = 0; lin < MACRO_LINHA; lin++) {
+		for (col = 0; col < MACRO_COLUNA; col++) {
+			//N=0
+			macro[lin] = matriz[lin];
+			macro[lin][col] = matriz[(lin + (MACRO_LINHA * (n / COLFIL)))][col + (MACRO_COLUNA*n%COLFIL)];
+
+		}
+	}
 
 }//fim macroBloco()
